3911ece22d0693f1913005a2f02e3a29d003d166
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-19 17:14:13 +0800

    :construction: Setup TailwindCSS

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -45,6 +45,7 @@ export default {
 	[
 		// https://go.nuxtjs.dev/typescript
 		'@nuxt/typescript-build',
+		'@nuxt/postcss8',
 	],
 
 	// Modules: https://go.nuxtjs.dev/config-modules
@@ -66,6 +67,13 @@ export default {
 	// Build Configuration: https://go.nuxtjs.dev/config-build
 	build:
 	{
-		//...
-	}
+		postcss:
+		{
+			plugins:
+			{
+				tailwindcss: {},
+				autoprefixer: {},
+			},
+		}
+	},
 }
---
+++ frontend/pages/test.vue
@@ -0,0 +1,14 @@
+<template>
+    <h1 class="text-blue-400 font-bold">
+        Hello world!
+    </h1>
+</template>
+
+<script>
+
+export default
+{
+    name  : 'TestPage',
+    layout: "dashboard",
+}
+</script>
