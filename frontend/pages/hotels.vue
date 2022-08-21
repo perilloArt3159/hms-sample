d28e25bacca9aa0f4d9523bc2e834f585550f543
zen <artperillo.ictdu@spcf.edu.ph>
2022-08-21 20:28:47 +0800

    :sparkles: Hotels Tailwind Table Design

--- frontend/pages/hotels.vue
+++ frontend/pages/hotels.vue
@@ -1,291 +1,72 @@
 <template>
     <div>
-        <h3 class="font-weight-bold text-secondary">
-            Hotels
-        </h3>
+        <h2 
+			class="font-bold text-gray-400 text-3xl my-3 tracking-wider capitalize"
+		>
+			hotels
+		</h2>
 
-        <hr class="hr" />
+		<hr/>
 
-        <template v-if="items!=null">
-            <b-container fluid>
-                <div>
-                    <b-button class="d-flex align-items-center" variant="primary" v-b-modal.modal-create>
-                        <i class="bi bi-plus-circle">
-
-                        </i>
-                        <span class="ml-2">
-                            Create Hotel
-                        </span>
-                    </b-button>
-
-                    <!-- MODAL CREATE -->
-                    <b-modal 
-                        id="modal-create" 
-                        title="Add Hotel" 
-                        @show="resetModalCreate" 
-                        @hidden="resetModalCreate"
-                        @ok="handleModalCreateOk"
-                    >
-                        <form 
-                            ref="formCreate" 
-                            @submit.stop.prevent="handleFormCreateSubmit"
+		<div 
+            v-if="items"
+			class="my-5"
+		>
+			
+            <div>
+                <table class="table-auto w-full rounded overflow-hidden">
+                    <thead>
+                        <tr class="bg-blue-500 text-white tracking-widest capitalize text-left font-bold">
+                            <th class="p-3">#</th>
+                            <th class="p-3 font-bold">name</th>
+                            <th class="p-3">address</th>
+                            <th class="p-3">created at</th>
+                            <th class="p-3">actions</th>
+                        </tr>
+                    </thead>
+                    <tbody class="bg-white text-sm tracking-wider">
+                        <tr class="border-y">
+                            <td class="p-2" colspan="4"> 
+                                
+                            </td>
+                            <td class="p-2">
+                                <button class="bg-green-500 block rounded w-full font-bold p-1">
+                                    <i class="bi bi-plus text-xl text-white rounded"></i>
+                                </button>
+                            </td>
+                        </tr>
+                        <tr 
+                            class="border-y-2"
+                            v-for="(item, index) in items.data.items" 
+                            :key="item.slug"
                         >
-                            <b-form-group 
-                                label="Name" 
-                                label-for="name-input" 
-                                invalid-feedback="Name is required"
-                                :state="formCreate.state.name"
-                            >
-                                <b-form-input 
-                                    id="name-input" 
-                                    v-model="formCreate.data.name" 
-                                    placeholder="Enter Name"
-                                    required 
-                                    :state="formCreate.state.name"
-                                >
-                                </b-form-input>
-                            </b-form-group>
-                            <b-form-group 
-                                label="Address" 
-                                label-for="address-input"
-                                invalid-feedback="Address is required" 
-                                :state="formCreate.state.address"
-                            >
-                                <b-form-textarea 
-                                    id="address-input" 
-                                    v-model="formCreate.data.address" 
-                                    rows="3"
-                                    placeholder="Enter Address..." 
-                                    required 
-                                    :state="formCreate.state.address"
-                                >
-                                </b-form-textarea>
-                            </b-form-group>
-                        </form>
-                    </b-modal>
-                    <!-- END MODAL CREATE -->
-
-
-                    <!-- MODAL UPDATE -->
-                    <b-modal 
-                        id="modal-update" 
-                        title="Update Hotel" 
-                        @hidden="resetModalUpdate"
-                        @ok="handleModalUpdateOk"
-                    >
-                        <form ref="formUpdate" @submit.stop.prevent="handleFormUpdateSubmit">
-                            <b-form-group 
-                                label="Name" 
-                                label-for="name-input" 
-                                invalid-feedback="Name is required"
-                                :state="formUpdate.state.name"
-                            >
-                                <b-form-input 
-                                    id="name-input" 
-                                    v-model="formUpdate.data.name" 
-                                    placeholder="Enter Name"
-                                    required 
-                                    :state="formUpdate.state.name"
-                                >
-                                </b-form-input>
-                            </b-form-group>
-                            <b-form-group 
-                                label="Address" 
-                                label-for="address-input"
-                                invalid-feedback="Address is required" 
-                                :state="formUpdate.state.address"
-                            >
-                                <b-form-textarea 
-                                    id="address-input" 
-                                    v-model="formUpdate.data.address" 
-                                    rows="3"
-                                    placeholder="Enter Address..." 
-                                    required 
-                                    :state="formUpdate.state.address"
-                                >
-                                </b-form-textarea>
-                            </b-form-group>
-                        </form>
-                    </b-modal>
-                    <!-- END MODAL UPDATE -->
-
-                </div>
-
-                <!-- User Interface controls -->
-                <b-row class="my-4">
-                    <b-col lg="6" class="my-1">
-                        <b-form-group 
-                            label="Sort" 
-                            label-for="sort-by-select" 
-                            label-cols-sm="2" 
-                            label-align-sm="center"
-                            label-class="font-weight-bold"
-                            label-size="sm" 
-                            class="mb-0" 
-                            v-slot="{ ariaDescribedby }"
-                        >
-                            <b-input-group size="sm">
-                                <b-form-select 
-                                    id="sort-by-select" 
-                                    v-model="sortBy" 
-                                    class="w-75"
-                                    :options="sortOptions"
-                                    :aria-describedby="ariaDescribedby" 
-                                    @change="onPageChange"
-                                >
-                                    <template #first>
-                                        <option value="">-- none --</option>
-                                    </template>
-                                </b-form-select>
-
-                                <b-form-select 
-                                    v-model="sortDesc" 
-                                    size="sm" 
-                                    class="w-25"
-                                    :disabled="!sortBy"
-                                    :aria-describedby="ariaDescribedby" 
-                                    @change="onPageChange"
-                                >
-                                    <option :value="false">Asc</option>
-                                    <option :value="true">Desc</option>
-                                </b-form-select>
-                            </b-input-group>
-                        </b-form-group>
-                    </b-col>              
-
-                    <b-col md="6" class="my-1">
-                        <b-form-group 
-                            label="Show" 
-                            label-for="per-page-select" 
-                            label-cols-sm="5" 
-                            label-cols-md="4"
-                            label-cols-lg="3" 
-                            label-class="font-weight-bold"
-                            label-align-sm="center" 
-                            label-size="sm" 
-                            class="mb-0"
-                        >
-                            <b-form-select 
-                                id="per-page-select" 
-                                v-model="paginationInfo.perPage" 
-                                size="sm"
-                                :options="pageOptions" 
-                                @change="onPageChange"
-                            >
-                            </b-form-select>
-                        </b-form-group>
-                    </b-col>
-
-                    <b-col lg="6" class="my-1">
-                        <b-form-group 
-                            label="Filter" 
-                            label-for="filter-input" 
-                            label-cols-sm="2"
-                            label-class="font-weight-bold"
-                            label-align-sm="center"
-                            label-size="sm" 
-                            class="mb-0"
-                        >
-                            <b-input-group size="sm">
-                                <b-form-input 
-                                    id="filter-input" 
-                                    v-model="filter" 
-                                    type="search"
-                                    placeholder="Type to Search"
-                                >
-                                </b-form-input>
-
-                                <b-input-group-append>
-                                    <b-button :disabled="!filter" @click="filter = null">Clear</b-button>
-                                </b-input-group-append>
-                            </b-input-group>
-                        </b-form-group>
-                    </b-col>
-
-                    <b-col sm="7" md="6"  class="my-1">
-                        <b-pagination 
-                            v-model="paginationInfo.currentPage" 
-                            class="my-0"
-                            align="fill" 
-                            size="sm" 
-                            :total-rows="items.data.pagination.total"
-                            :per-page="items.data.pagination.size" 
-                            @change="onPageChange"
-                        >
-                        </b-pagination>
-                    </b-col>
-                </b-row>
-
-                <!-- Main table element -->
-                <b-table 
-                    :items="items.data.items" 
-                    :fields="fields" 
-                    :current-page="paginationInfo.currentPage"
-                    :per-page="paginationInfo.size" 
-                    :filter="filter" 
-                    :sort-by.sync="sortBy" 
-                    :sort-desc.sync="sortDesc" 
-                    :sort-direction="sortDirection" 
-                    stacked="md"
-                    show-empty 
-                    @filtered="onFiltered"
-                >
-
-                    <!-- BUSY STATE -->
-                    <template #table-busy>
-                        <div class="text-center text-danger my-2">
-                            <b-spinner class="align-middle" variant="primary" />
-                            <strong class="text-primary">Loading...</strong>
-                        </div>
-                    </template>
-                    <!-- END BUSY STATE -->
-
-                    <!-- A virtual column -->
-                    <template #cell(#)="row">
-                        {{ row.index + 1 }}
-                    </template>
-
-                    <template #cell(key)="row">
-                        {{ row.value.first }} {{ row.value.last }}
-                    </template>
-
-                    <template #cell(actions)="row">
-                        <b-button 
-                            size="sm" 
-                            class="mr-1" 
-                            variant="info" 
-                            @click="setupFormUpdate(row.item)" 
-                            v-b-modal.modal-update
-                        >
-                            <i class="bi bi-pen-fill fs-6"></i>
-                        </b-button>
-                        <b-button size="sm" variant="danger" @click="deleteItem(row.item.slug)">
-                            <i class="bi bi-trash-fill fs-6"></i>
-                        </b-button>
-                    </template>
-
-                    <template #row-details="row">
-                        <b-card>
-                            <ul>
-                                <li v-for="(value, key) in row.item" :key="key">
-                                    {{ key }}: {{ value }}
-                                </li>
-                            </ul>
-                        </b-card>
-                    </template>
-                </b-table>
-
-            </b-container>
-        </template>
-
-        <template v-else>
-            <div class="text-center my-5 d-flex justify-content-center">
-                <b-spinner variant="primary" label="Spinning" style="width: 4rem; height: 4rem;">
-                </b-spinner>
+                            <td class="p-2"> 
+                                {{ index + 1 }}
+                            </td>
+                            <td class="p-2">
+                                {{ item.name }}
+                            </td>
+                            <td class="p-2">
+                                {{ item.address }}
+                            </td>
+                            <td class="p-2">
+                                {{ item.createdAt }}
+                            </td>
+                            <td class="grid grid-cols-2 gap-2 p-2">
+                                <button class="bg-blue-500 rounded p-1 font-bold">
+                                    <i class="bi bi-pen-fill text-xl text-white rounded"></i>
+                                </button>
+                                <button class="bg-red-500 rounded p-1">
+                                    <i class="bi bi-trash-fill text-xl text-white rounded"></i>
+                                </button>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
-        </template>
 
 
+		</div>
     </div>
 </template>
 
